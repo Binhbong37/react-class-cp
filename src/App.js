d26f38201ff2a536dc15166a3a5213b668c00a5b
Hoang Van Binh <binhhvFX12117@funix.edu.vn>
2022-05-31 15:19:27 +0700

    2. React cp2, truyền dữ liệu từ cha xuống, ấn vào hiện chi tiết cho mỗi mục

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 
+import { DISHES } from './shared/dishes';
+
 import Menu from './Components/MenuComponent';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+        };
+    }
     render() {
         return (
             <div className="App">
@@ -15,7 +23,7 @@ class App extends Component {
                         </NavbarBrand>
                     </div>
                 </Navbar>
-                <Menu />
+                <Menu dishes={this.state.dishes} />
             </div>
         );
     }
--- src/Components/FuncComponet.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const FuncComponet = () => {
-    return <div>FuncComponet</div>;
-};
-
-export default FuncComponet;
